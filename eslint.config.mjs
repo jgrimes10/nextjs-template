cbc5cbb3c23280639d8e83047cf0d0f8531c0d49
jgrimes10 <grimesajustin@gmail.com>
2025-02-22 12:34:03 -0500

    Base setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import checkFile from "eslint-plugin-check-file";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,42 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+    },
+  },
+  {
+    files: ["**/*.yaml"],
+    processor: "check-file/eslint-processor-check-file",
+  },
+  {
+    files: ["src/**/*.*"],
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
