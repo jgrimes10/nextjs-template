82577d3a7301d4e2af7404803384376e2f97c8e2
jgrimes10 <grimesajustin@gmail.com>
2025-02-22 14:18:31 -0500

    Add dark mode. Update styling.

---
+++ src/components/providers.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { JSX, ReactNode } from "react";
+
+import { HeroUIProvider } from "@heroui/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+/**
+ * Set up the HeroUIProvider.
+ * @param {ReactNode} children The children of the provider.
+ * @returns {JSX.Element} The provider.
+ */
+export default function Providers({
+  children,
+}: {
+  children: ReactNode;
+}): JSX.Element {
+  const router = useRouter();
+  return (
+    <HeroUIProvider
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      navigate={router.push}
+      className="flex h-full w-full flex-col"
+    >
+      <NextThemesProvider attribute="class">{children}</NextThemesProvider>
+    </HeroUIProvider>
+  );
+}
